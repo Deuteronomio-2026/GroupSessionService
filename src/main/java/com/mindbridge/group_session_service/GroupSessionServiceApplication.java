0c0c11042de91edffd7aa3d2846ca214032b5f70
Jacoboo <jacobo.diaz-a@mail.escuelaing.edu.co>
2026-04-26 23:00:09 -0500

    Primer commit

---
+++ src/main/java/com/mindbridge/group_session_service/GroupSessionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mindbridge.group_session_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupSessionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupSessionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindbridge/group_session_service/GroupSessionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindbridge.group_session_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupSessionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
